2e7c1d697c28ec9d1af4b3de4cb971b7da2c2f98
Andy Lopez <profesor.de.ingles@aol.com>
2021-03-09 15:35:33 -0500

    ADDED | Function that finds the lowest common ancestor of two nodes

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_ancestor - Finds the lowest common ancestor of two nodes.
+ *
+ *     Arguments:
+ *       @first:        - Pointer to first node to compare.
+ *       @second:       - Pointer to second node to compare.
+ *       Return:        - Pointer to the common ancestor, NULL if not found.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *ancestor;
+
+	if (!first || !second)
+		return (1);
+	ancestor = (binary_tree_t *)second;
+	while (first)
+	{
+		while (second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)first);
+			second = second->parent;
+		}
+		second = ancestor;
+		first = first->parent;
+	}
+	return (NULL);
+}
