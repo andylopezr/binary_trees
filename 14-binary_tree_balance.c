48c924dc8ba4cb10d4ba74f1acd7c799e121be18
DiegoCol93 <dfelo93@gmail.com>
2021-03-10 10:36:38 -0500

    UPDATED | New height function with ternary return outside of balance function.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,14 +12,36 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int l_height = 0, r_height = 0, balance = 0;
 
+	if (!tree)
+		return (0);
+	if (tree->left)
+		l_height = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		r_height = binary_tree_height(tree->right) + 1;
+	balance = l_height - r_height;
+	return (balance);
+}
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ *
+ *      Arguments:
+ *        @tree:      - Pointer to the root of the tree.
+ *
+ *        Return:     - Height of the given tree.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height = 0, r_height = 0;
+
 	if (tree)
 	{
 		if (tree->left)
-			l_height = binary_tree_balance(tree->left) + 1;
+			l_height = binary_tree_height(tree->left) + 1;
 		if (tree->right)
-			r_height = binary_tree_balance(tree->right) + 1;
-		balance = l_height - r_height;
-		return (balance);
+			r_height = binary_tree_height(tree->right) + 1;
+
+		return (r_height >= l_height ? r_height : l_height);
 	}
 	else
 		return (0);
