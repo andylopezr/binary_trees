3ce5f2a567923729ea5564820f4088ff81ffab6a
Andy Lopez <profesor.de.ingles@aol.com>
2021-03-10 21:38:19 -0500

    FIXED | style.

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 #include <stdio.h>
+
 /**
  * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search Tree.
  *      Arguments:
@@ -7,12 +8,22 @@
  *
  *       Return:      - 1 if a valid BST, 0 otherwise.
  */
+
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 	return (bst_helper(tree, INT_MIN, INT_MAX));
 }
+
+/**
+ * bst_helper - Checks if a binary tree is a valid Binary Search Tree.
+ *      Arguments:
+ *        @tree:      - Pointer to the root of the tree.
+ *       @lesser:     - To analyze lesser than root value.
+ *      @greater:     - To analyze greater than root value.
+ *       Return:      - 1 if a valid BST, 0 otherwise.
+ */
 
 int bst_helper(const binary_tree_t *tree, int lesser, int greater)
 {
