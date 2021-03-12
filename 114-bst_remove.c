db59211d1393d42d4d72334600bafd969daefec0
Andy Lopez <profesor.de.ingles@aol.com>
2021-03-11 22:08:22 -0500

    FIXED | conflicts

---
+++ 114-bst_remove.c
@@ -0,0 +1,100 @@
+#include "binary_trees.h"
+/**
+<<<<<<< HEAD
+ * bst_remove - Searches for a value in a Binary Search Tree.
+=======
+ * bst_remove - Removes a node from a BST.
+>>>>>>> 3bf29114fe5902114c5317df5e6867394547c786
+ *
+ *  Arguments:
+ *    @root:  - Pointer to the BST to analyze.
+ *    @value: - Value to remove from BST.
+ *
+ *    Return: - Pointer to new root.
+ */
+
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *temp = NULL, *leaf = NULL;
+
+	leaf = bst_search(root, value);
+	if (!leaf)
+		return (root);
+	if (leaf->left && !leaf->right)
+	{
+<<<<<<< HEAD
+		for (temp = leaf->left; temp->right; temp = temp->right)
+			;
+=======
+		/* temp = leaf->left; */
+		/* while (temp->right) */
+		/* 	temp = temp->right; */
+		for (temp = leaf->left; temp->right; temp = temp->right);
+>>>>>>> 3bf29114fe5902114c5317df5e6867394547c786
+		if (temp != leaf->left)
+		{
+			temp->parent->right = temp->left;
+			temp->left = leaf->left;
+		}
+		temp->right = leaf->right, temp->parent = leaf->parent;
+	}
+	else if (leaf->right)
+	{
+<<<<<<< HEAD
+		for (temp = leaf->right; temp->left; temp = temp->left)
+			;
+=======
+		/* temp = leaf->right; */
+		/* while (temp->left) */
+		/* 	temp = temp->left; */
+		for (temp = leaf->right; temp->left; temp = temp->left);
+>>>>>>> 3bf29114fe5902114c5317df5e6867394547c786
+		if (temp != leaf->right)
+		{
+			temp->parent->left = temp->right;
+			temp->right = leaf->right;
+		}
+		temp->left = leaf->left, temp->parent = leaf->parent;
+	}
+	if (leaf->parent && leaf->parent->right == leaf)
+		leaf->parent->right = temp;
+	if (leaf->parent && leaf->parent->left == leaf)
+		leaf->parent->left = temp;
+	free(leaf);
+	if (temp->right && temp)
+		temp->right->parent = temp;
+	if (temp->left && temp)
+		temp->left->parent = temp;
+	if (!temp->parent)
+		return (temp);
+	return (root);
+}
+
+/**
+ * bst_search - Searches for a value in a Binary Search Tree.
+ *
+ *  Arguments:
+ *    @tree:  - Pointer to the BST to analyze.
+ *    @value: - Value to find in BST.
+ *
+ *    Return: - Pointer to the matching node.
+ */
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	bst_t *node = NULL;
+
+	if (!tree)
+		return (NULL);
+	node = (bst_t *)tree;
+	while (node)
+	{
+		if (node->n == value)
+			return (node);
+		if (node->n < value)
+			node = node->right;
+		else
+			node = node->left;
+	}
+	return (NULL);
+}
